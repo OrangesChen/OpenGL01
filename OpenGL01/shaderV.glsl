d8611414c148961ad1b8f663401158ecbc8e6f95
OrangesChen <1345341069@qq.com>
2019-12-29 22:15:14 +0800

    update

--- OpenGL01/shaderV.glsl
+++ OpenGL01/shaderV.glsl
@@ -5,6 +5,10 @@ attribute vec2 vertTexCoord;
 //这个值需要和片段着色器的值相同
 varying vec2 fragTexCoord;
 
+// const 用于声明非可写的编译时常量变量
+// attribute 用于经常更改的信息，只能在顶点着色器中使用
+// uniform 用于不经常更改的信息，可用于顶点着色器和片元着色器
+// varying 用于修饰从顶点着色器向片元着色器传递的变量
 
 //layout(location = 0) out vec3 fragTexture;
 
